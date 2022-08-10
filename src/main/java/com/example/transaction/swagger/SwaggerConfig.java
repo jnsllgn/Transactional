3ffda2aff279fb0fa946491b9d274f43b86f3e77
Juan Marihot Siallagan <C020822016@Supporting.corp.bankmandiri.co.id>
2022-08-10 10:53:29 +0700

    Swagger

--- src/main/java/com/example/transaction/swagger/SwaggerConfig.java
+++ src/main/java/com/example/transaction/swagger/SwaggerConfig.java
@@ -11,6 +11,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+//     Dokumentasikan API
+//     http://localhost:8080/swagger-ui/#/ka
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
